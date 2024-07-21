2dac75b87c1e18529923124c93b9aa5fce496970
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-07-21 15:16:05 +0500

    changes in skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,7 +6,7 @@ function Skills() {
   return (
     <section className='skills section ' id='skills'>
       <h2 className="section__title">Skills</h2>
-      <span className='section__subtitle'>My Introduction</span>
+      <span className='section__subtitle'>My Technical Level</span>
 
       <div className="skill__container container grid">
         <FrontEnd />
